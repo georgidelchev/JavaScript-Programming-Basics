f58ebbd35bc6f8a7cd6277af7770f3b2047ce057
george <alienguymc@gmail.com>
2020-10-26 14:48:20 +0200

    add

--- 02 - [First Steps In Coding - Exercise]/P01UsdToBgn.js
+++
@@ -1,9 +0,0 @@
-function usdToBgn(usd) {
-    let usdMoney = Number(usd);
-    let convertedToBgn = usdMoney * 1.79549;
-    console.log(convertedToBgn);
-}
-
-usdToBgn(22);
-usdToBgn(100);
-usdToBgn(12.5);
